9224149067e26b782baa24407ade9b6b6849b797
Cheng Lou <chenglou92@gmail.com>
2016-01-10 23:05:27 -0500

    Rename stuff to make file names more readable

--- test.sh
+++ test.sh
@@ -1,85 +1,85 @@
-ocamlopt accessNoiseSmall.ml -o accessNoiseSmall.native
-ocamlopt accessRecSmall.ml -o accessRecSmall.native
-ocamlopt accessObjSmall.ml -o accessObjSmall.native
+ocamlopt access_small_noise.ml -o access_small_noise.native
+ocamlopt access_small_rec.ml -o access_small_rec.native
+ocamlopt access_small_obj.ml -o access_small_obj.native
 
-ocamlopt accessNoiseBig.ml -o accessNoiseBig.native
-ocamlopt accessRecBig.ml -o accessRecBig.native
-ocamlopt accessObjBig.ml -o accessObjBig.native
+ocamlopt access_big_noise.ml -o access_big_noise.native
+ocamlopt access_big_rec.ml -o access_big_rec.native
+ocamlopt access_big_obj.ml -o access_big_obj.native
 
-ocamlopt mutHeapValueAccessNoise.ml -o mutHeapValueAccessNoise.native
-ocamlopt mutHeapValueAccessRec.ml -o mutHeapValueAccessRec.native
-ocamlopt mutHeapValueAccessObj.ml.ml -o mutHeapValueAccessObj.native
+ocamlopt access_mutHeapValue_noise.ml -o access_mutHeapValue_noise.native
+ocamlopt access_mutHeapValue_rec.ml -o access_mutHeapValue_rec.native
+ocamlopt access_mutHeapValue_obj.ml -o access_mutHeapValue_obj.native
 
-ocamlopt accessNoiseCachedSmall.ml -o accessNoiseCachedSmall.native
-ocamlopt accessObjCachedSmall.ml -o accessObjCachedSmall.native
-ocamlopt accessObjNonCachedSmall.ml -o accessObjNonCachedSmall.native
+ocamlopt access_obj_small_noise.ml -o access_obj_small_noise.native
+ocamlopt access_obj_small_cached.ml -o access_obj_small_cached.native
+ocamlopt access_obj_small_noncached.ml -o access_obj_small_noncached.native
 
-ocamlopt accessNoiseCachedBig.ml -o accessNoiseCachedBig.native
-ocamlopt accessObjCachedBig.ml -o accessObjCachedBig.native
-ocamlopt accessObjNonCachedBig.ml -o accessObjNonCachedBig.native
+ocamlopt access_obj_big_noise.ml -o access_obj_big_noise.native
+ocamlopt access_obj_big_cached.ml -o access_obj_big_cached.native
+ocamlopt access_obj_big_noncached.ml -o access_obj_big_noncached.native
 
-ocamlopt accessNoiseOpen.ml -o accessNoiseOpen.native
-ocamlopt accessObjOpen.ml -o accessObjOpen.native
-ocamlopt accessObjClosed.ml -o accessObjClosed.native
+ocamlopt access_obj_openclosed_noise.ml -o access_obj_openclosed_noise.native
+ocamlopt access_obj_openclosed_open.ml -o access_obj_openclosed_open.native
+ocamlopt access_obj_openclosed_closed.ml -o access_obj_openclosed_closed.native
 
-ocamlopt createNoiseSmall.ml -o createNoiseSmall.native
-ocamlopt createRecSmall.ml -o createRecSmall.native
-ocamlopt createObjSmall.ml -o createObjSmall.native
+ocamlopt create_small_noise.ml -o create_small_noise.native
+ocamlopt create_small_rec.ml -o create_small_rec.native
+ocamlopt create_small_obj.ml -o create_small_obj.native
 
-ocamlopt createNoiseBig.ml -o createNoiseBig.native
-ocamlopt createRecBig.ml -o createRecBig.native
-ocamlopt createObjBig.ml -o createObjBig.native
+ocamlopt create_big_noise.ml -o create_big_noise.native
+ocamlopt create_big_rec.ml -o create_big_rec.native
+ocamlopt create_big_obj.ml -o create_big_obj.native
 
-ocamlopt updateNoiseSmall.ml -o updateNoiseSmall.native
-ocamlopt updateRecSmall.ml -o updateRecSmall.native
-ocamlopt updateObjSmall.ml -o updateObjSmall.native
+ocamlopt update_small_noise.ml -o update_small_noise.native
+ocamlopt update_small_rec.ml -o update_small_rec.native
+ocamlopt update_small_obj.ml -o update_small_obj.native
 
-ocamlopt updateNoiseBig.ml -o updateNoiseBig.native
-ocamlopt updateRecBig.ml -o updateRecBig.native
-ocamlopt updateObjBig.ml -o updateObjBig.native
+ocamlopt update_big_noise.ml -o update_big_noise.native
+ocamlopt update_big_rec.ml -o update_big_rec.native
+ocamlopt update_big_obj.ml -o update_big_obj.native
 
 echo "=========start benchmarks========="
 
 echo "\naccess time, small record vs obj (cached)"
-./accessNoiseSmall.native
-./accessRecSmall.native
-./accessObjSmall.native
+./access_small_noise.native
+./access_small_rec.native
+./access_small_obj.native
 echo "\naccess time, big record vs obj (cached)"
-./accessNoiseBig.native
-./accessRecBig.native
-./accessObjBig.native
+./access_big_noise.native
+./access_big_rec.native
+./access_big_obj.native
 echo "\naccess time with mutable + heap allocated value, record vs obj (cached)"
-./mutHeapValueAccessNoise.native
-./mutHeapValueAccessRec.native
-./mutHeapValueAccessObj.native
+./access_mutHeapValue_noise.native
+./access_mutHeapValue_rec.native
+./access_mutHeapValue_obj.native
 echo "\naccess time, obj cached vs uncached, small"
-./accessNoiseCachedSmall.native
-./accessObjCachedSmall.native
-./accessObjNonCachedSmall.native
+./access_obj_small_noise.native
+./access_obj_small_cached.native
+./access_obj_small_noncached.native
 echo "\naccess time, obj cached vs uncached, big"
-./accessNoiseCachedBig.native
-./accessObjCachedBig.native
-./accessObjNonCachedBig.native
+./access_obj_big_noise.native
+./access_obj_big_cached.native
+./access_obj_big_noncached.native
 echo "\naccess time, open vs closed obj"
-./accessNoiseOpen.native
-./accessObjOpen.native
-./accessObjClosed.native
+./access_obj_openclosed_noise.native
+./access_obj_openclosed_open.native
+./access_obj_openclosed_closed.native
 echo "\ncreation time, small record vs obj"
-./createNoiseSmall.native
-./createRecSmall.native
-./createObjSmall.native
+./create_small_noise.native
+./create_small_rec.native
+./create_small_obj.native
 echo "\ncreation time, big record vs obj"
-./createNoiseBig.native
-./createRecBig.native
-./createObjBig.native
+./create_big_noise.native
+./create_big_rec.native
+./create_big_obj.native
 echo "\nupdate time, record vs obj, small"
-./updateNoiseSmall.native
-./updateRecSmall.native
-./updateObjSmall.native
+./update_small_noise.native
+./update_small_rec.native
+./update_small_obj.native
 echo "\nupdate time, record vs obj, big"
-./updateNoiseBig.native
-./updateRecBig.native
-./updateObjBig.native
+./update_big_noise.native
+./update_big_rec.native
+./update_big_obj.native
 
 rm *.cmi
 rm *.cmx
